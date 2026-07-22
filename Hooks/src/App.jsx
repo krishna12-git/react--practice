d169de5812fb26265a39b9abebd7d7eeb7905e73
krishna12 <krishyadav7856@gmail.com>
2026-07-22 12:58:32 +0530

    understanding usestate with example of counter

---
+++ Hooks/src/App.jsx
@@ -0,0 +1,32 @@
+// import React, { useState } from 'react'
+
+// const App = () => {
+
+//     const [num, setnum] = useState(0)
+
+//     function increaaeNum(){
+//         setnum(num+1)
+//     }
+
+//      function decreaaeNum(){
+//         setnum(num-1)
+//     }
+
+//     function jump5Num(){
+//         setnum(num+5)
+//     }
+//   return (
+//     <div>
+//         <h1>{num} </h1>
+//         <button onClick={increaaeNum}>increase</button>
+//         <button onClick={decreaaeNum}>decrease</button>
+//         <button onClick={jump5Num}> jump by 5</button>
+//     </div>
+//   )
+// }
+
+
+// export default App
+// understanding usestate with example of counter
+
+
