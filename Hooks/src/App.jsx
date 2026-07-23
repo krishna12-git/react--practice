815adbfa9acb4afd5ea143826f20381bfd109620
krishna12 <krishyadav7856@gmail.com>
2026-07-23 23:27:58 +0530

    changing my name

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -29,40 +29,68 @@
 // export default App
 // understanding usestate with example of counter
 
-import React, {useState} from 'react'
+// ***************************************************************************************************
+ 
+// eg of changing your name by clicking by using if else statement
+
+// import React, {useState} from 'react'
+
+// const App = () => {
+
+//     const [person, setPerson] = useState({
+//         name: "Krishna",
+//         age: 20
+//     });
+
+//     const changePerson = () => {
+
+//         if (person.name === "Krishna"){
+//             setPerson({
+//             name: "Rahul",
+//             age: 25
+//         });
+//         } else {
+//             setPerson({
+//                 name: "Krishna",
+//                 age: 20,
+//             });
+//         }
+       
+//     };
+
+    
+//   return (
+//     <div>
+//         <h1>Hi! {person.name} , {person.age} </h1>
+//         <button onClick={changePerson}>click</button>
+//     </div>
+//   )
+// }
+
+// export default App
+// ***************************************************************************************************************************************************************************************************************************************************
+
+import React, { useState } from 'react'
 
 const App = () => {
 
-    const [person, setPerson] = useState({
-        name: "Krishna",
-        age: 20
-    });
-
-    const changePerson = () => {
-
-        if (person.name === "Krishna"){
-            setPerson({
-            name: "Rahul",
-            age: 25
-        });
-        } else {
-            setPerson({
-                name: "Krishna",
-                age: 20,
-            });
-        }
-       
-    };
-
+    const [num, setNum] = useState({user:'krishna',age:20})
     
+    const btnClicked = () => {
+        const newNum = {...num};
+        newNum.user = 'Aman'
+        newNum.age = 23
+        setNum(newNum)
+
+    }
+
+
   return (
     <div>
-        <h1>Hi! {person.name} , {person.age} </h1>
-        <button onClick={changePerson}>click</button>
-    </div>
+        <h1> {num.user}, {num.age}</h1>
+        <button onClick={btnClicked}>click</button>
+     </div>
   )
 }
 
 export default App
-
-
