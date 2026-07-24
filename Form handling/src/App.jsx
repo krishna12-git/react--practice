6720534baea29f7761ac39a08a510d398308c64f
krishna12 <krishyadav7856@gmail.com>
2026-07-24 22:56:53 +0530

    practicing, when i clicked on submit btn in console i should see the welcome+name

--- Form handling/src/App.jsx
+++ Form handling/src/App.jsx
@@ -1,21 +1,51 @@
-import React from 'react'
+// import React from 'react'
+
+// const App = () => {
+
+  
+
+// const submitHandler = (e) => {
+//   e.preventDefault() // without preventDefaulter the page will refresh and everything will disappear
+//   console.log('form submitted');
+  
+// }  
+//   return (
+//     <div>
+//       <form  onSubmit={(e) => 
+//         submitHandler(e)
+//       }>
+//         <input type="text" placeholder='Enter your name' />
+//         <button>Submit</button>
+//         </form>
+//       </div>
+//   )
+// }
+
+// export default App
+
+// ***************************************************************************************************************
+
+
+import { useState } from 'react'
 
 const App = () => {
 
-const submitHandler = (e) => {
-  e.preventDefault() // without preventDefaulter the page will refresh and everything will disappear
-  console.log('form submitted');
-  
-}  
+  const [name, setName] = useState("")
+
+  const btnClicked = (e) => {
+    e.preventDefault();
+    console.log("Welcome", name);  
+  }
+
   return (
     <div>
-      <form  onSubmit={(e) => 
-        submitHandler(e)
-      }>
-        <input type="text" placeholder='Enter your name' />
-        <button>Submit</button>
-        </form>
-      </div>
+      <form onSubmit={btnClicked}>
+         <input type="text" placeholder='Enter your name'
+         value={name}
+         onChange={(e) => setName(e.target.value)}/>
+         <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
